147356f1d3147605c3a1cfa2d334fa64cd7fdec5
The-Boat <radair2842@gmail.com>
2019-06-20 12:25:21 +0100

    First version of view forms.
    - Engineer
    - Invoice
    - Customer

--- frmViewCustomers.Designer.cs
+++ frmViewCustomers.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.cbxFilters = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.btnNewInvoice = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.clbCustomers = new System.Windows.Forms.CheckedListBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(432, 135);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(39, 17);
+            this.lblFilter.TabIndex = 13;
+            this.lblFilter.Text = "Filter";
+            // 
+            // cbxFilters
+            // 
+            this.cbxFilters.FormattingEnabled = true;
+            this.cbxFilters.Location = new System.Drawing.Point(484, 132);
+            this.cbxFilters.Name = "cbxFilters";
+            this.cbxFilters.Size = new System.Drawing.Size(153, 24);
+            this.cbxFilters.TabIndex = 12;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(643, 83);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(145, 38);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxSearch.Location = new System.Drawing.Point(12, 83);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(625, 38);
+            this.tbxSearch.TabIndex = 10;
+            // 
+            // btnNewInvoice
+            // 
+            this.btnNewInvoice.Location = new System.Drawing.Point(643, 433);
+            this.btnNewInvoice.Name = "btnNewInvoice";
+            this.btnNewInvoice.Size = new System.Drawing.Size(145, 41);
+            this.btnNewInvoice.TabIndex = 9;
+            this.btnNewInvoice.Text = "New Customer";
+            this.btnNewInvoice.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(492, 433);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(145, 41);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit Customer";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // clbCustomers
+            // 
+            this.clbCustomers.FormattingEnabled = true;
+            this.clbCustomers.Location = new System.Drawing.Point(12, 170);
+            this.clbCustomers.Name = "clbCustomers";
+            this.clbCustomers.Size = new System.Drawing.Size(776, 242);
+            this.clbCustomers.TabIndex = 7;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(151, 32);
+            this.lblTitle.TabIndex = 14;
+            this.lblTitle.Text = "Customers";
+            // 
+            // frmViewCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(802, 503);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.cbxFilters);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxSearch);
+            this.Controls.Add(this.btnNewInvoice);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.clbCustomers);
+            this.Name = "frmViewCustomers";
             this.Text = "frmViewCustomers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.ComboBox cbxFilters;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.Button btnNewInvoice;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.CheckedListBox clbCustomers;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
--- frmViewInvoices.Designer.cs
+++ frmViewInvoices.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.clbInvoices = new System.Windows.Forms.CheckedListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNewInvoice = new System.Windows.Forms.Button();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clbInvoices
+            // 
+            this.clbInvoices.FormattingEnabled = true;
+            this.clbInvoices.Location = new System.Drawing.Point(12, 156);
+            this.clbInvoices.Name = "clbInvoices";
+            this.clbInvoices.Size = new System.Drawing.Size(776, 242);
+            this.clbInvoices.TabIndex = 0;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(492, 419);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(145, 41);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit Invoice";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnNewInvoice
+            // 
+            this.btnNewInvoice.Location = new System.Drawing.Point(643, 419);
+            this.btnNewInvoice.Name = "btnNewInvoice";
+            this.btnNewInvoice.Size = new System.Drawing.Size(145, 41);
+            this.btnNewInvoice.TabIndex = 2;
+            this.btnNewInvoice.Text = "New Invoice";
+            this.btnNewInvoice.UseVisualStyleBackColor = true;
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxSearch.Location = new System.Drawing.Point(12, 69);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(625, 38);
+            this.tbxSearch.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(643, 69);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(145, 38);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(119, 32);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Invoices";
+            // 
+            // frmViewInvoices
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(802, 503);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxSearch);
+            this.Controls.Add(this.btnNewInvoice);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.clbInvoices);
+            this.Name = "frmViewInvoices";
+            this.Text = "View Invoices";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox clbInvoices;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNewInvoice;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
