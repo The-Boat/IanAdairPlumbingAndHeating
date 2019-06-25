83bada23d5a4ea9d3d3ef73c434650d0ab962244
The-Boat <radair2842@gmail.com>
2019-06-25 12:53:34 +0100

    Merge

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace IanAdairPlumbingAndHeating
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmAddEngineer());
         }
     }
 }
--- Visit.cs
+++ Visit.cs
@@ -8,5 +8,6 @@ namespace IanAdairPlumbingAndHeating
 {
     class Visit
     {
+        
     }
 }
